6257d42b2cb03a5035f66ddda937b0172d1746ac
vivekmittal110 <vivekmittal110230@gmail.com>
2023-11-18 19:58:47 +0530

    first program

--- basic/00_first.cpp
+++ basic/00_first.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 using namespace std;
 int main(){
+    // Double slash is used to single line comment
+    /* this is used for 
+    mulltiline comments*/
+    
+    // cin >> is used to take input from user
+    // cout << is used to display or print
+
+    // valriabe -> variable is like a container which contain some vaules 
+    // data types -> int(1, 2, 3,....), float(1.2, 34.5,....), char('a','@','7'....), string("vivek",...), boolean(true,false), void(null)
+
+    //example
+    int num = 786;
+    cout << num <<endl; // endl is used for new line
+
+    float fl;
+    cin >> fl;
+    cout << fl;
     
     return 0;
 }
