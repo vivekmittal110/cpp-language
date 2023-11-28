9f6a1048f04452ec1e2a993af0c4e3dd1fcd371a
vivekmittal110 <vivekmittal110230@gmail.com>
2023-11-28 22:52:10 +0530

    arrays

---
+++ 04_arrays/00_basic.cpp
@@ -0,0 +1,9 @@
+// Array
+// data structure which stores a collection of homogenous items
+// arrays have contiguous memoey
+// 0 index
+
+// array deceleration
+// data_type array_name[array_size]
+// int arr[5];  // giving size is important
+// int arr[] = {1,2,3,4,5}; // giving size not mendatory
